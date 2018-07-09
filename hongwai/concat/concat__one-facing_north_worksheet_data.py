80cb66e6e9c9bae84a844e4ec6ad34f1cca3f46c
chen <951849311@qq.com>
2018-07-09 11:49:09 +0800

    concat data wall-one-facing north

---
+++ hongwai/concat/concat__one-facing_north_worksheet_data.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 13:51:48 2018
+
+@author: DELL
+"""
+
+# 此程序用于拼接红外温度数据
+# 对应高宽比为1:1的朝北侧壁
+
+import glob
+import pandas as pd
+
+
+ipath = r'E:\py_data\hongwai\one-facing north'
+#fname = glob.glob(ipath+'\\'+'*.xls*')
+fname = glob.glob(ipath+'\\'+'one-facing north-22.xlsx')
+
+result = []
+for f in fname:
+    all_worksheets = pd.read_excel(f,sheetname=None,skiprows=8,index_col=0) # 跳过8行来读取数据，以第一列为index（行的名字）
+    for worksheet_name,data in all_worksheets.items():
+        result.append(data.T)
+
+#result.reverse()
+final = pd.concat(result)
+final.to_csv('E:\py_output'+'\\'+'test.csv',index=False,header=None)
